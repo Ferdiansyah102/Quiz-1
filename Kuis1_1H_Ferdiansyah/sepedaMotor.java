84d9e7a247e55206a666dcd2398e36e3fd43eab1
Ferdiansyah102 <72367853+Ferdiansyah102@users.noreply.github.com>
2021-03-06 09:08:46 +0700

    Pengumpulan kuis 1

--- Kuis1_1H_Ferdiansyah/sepedaMotor.java
+++ Kuis1_1H_Ferdiansyah/sepedaMotor.java
@@ -11,5 +11,36 @@ package Kuis1_1H_Ferdiansyah;
  * @author ACER
  */
 public class sepedaMotor {
-    
+    public String merk;
+    public boolean kontakMotor;
+    public int kecepatanMotor;
+
+    void sepedaMotor(String honda, String yamaha, String suzuki, String ducati) {
+       
+    }
+    void SepedaMotor(String merk){
+        this.merk=merk;
+    }
+    void nyalakanMotor(boolean kontakMotor){
+        this.kontakMotor = kontakMotor;
+    }
+    void matikanMotor(boolean kontakMotor){
+        this.kontakMotor = kontakMotor;
+    }
+    void tambahKecepatanMotor(int kecepatanMotor){
+        kecepatanMotor=+ 5 ;
+    }
+    void kurangiKecepatan(){
+        kecepatanMotor =-5;
+    }
+    void info(){
+        System.out.println("=========================");
+        if(kontakMotor == true){
+            System.out.println("sepeda motor telah hidup");
+        }else{
+            System.out.println("sepeda motor mati");
+        }
+        System.out.println("kecepatan : " + kecepatanMotor);
+        System.out.println("=========================");
+    }
 }
